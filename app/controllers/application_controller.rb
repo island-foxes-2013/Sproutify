600e4f75fcca7f6fc1e82efe0f107fad025fbbd7
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-15 19:25:35 -0700

    Added authorization helpers and before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,28 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :require_login
+
+  def current_user=(user)
+    session[:user_id] = user.id
+  end
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  helper_method :current_user, :logged_in?
+
+  private 
+
+  def require_login
+    if !logged_in?
+      flash[:error] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
