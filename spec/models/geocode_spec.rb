78ece026945dd3f20242d0382a8532a36f405950
dbcapprentice <adamloo85@gmail.com>
2013-08-15 19:23:06 -0700

    added some geocode tests

--- spec/models/geocode_spec.rb
+++ spec/models/geocode_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Geocode do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it{should respond_to(:lat)}
+  it{should respond_to(:lng)}
+  it{should respond_to(:user)}
+
+  describe "#initialize" do
+
+    context "valid data" do
+      it "should save to database" do
+
+      end
+  end
 end
