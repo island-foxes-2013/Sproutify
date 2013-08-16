754c5ae29e7d363a17086eb6e783a3cabac228fc
Cole Pilegard <c.pilegard@gmail.com>
2013-08-16 15:36:40 -0700

    refactor geocode search in model

--- app/models/geocode.rb
+++ app/models/geocode.rb
@@ -3,11 +3,10 @@ class Geocode < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user, :lat, :lng
 
+  # sunspot search via :location
   searchable do
-  
     double :lat
     double :lng
     latlon(:location) { Sunspot::Util::Coordinates.new(lat, lng) }
-
   end
 end
