09aa7bd9de728651b679a44bb82761065a9247e3
Cole Pilegard <c.pilegard@gmail.com>
2013-08-15 17:04:06 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sproutify::Application.config.secret_token = 'f1e055000bf3be8ce95a4a3b2da9ee7389cc2ef35c8823753b962b772728e6b03ae9cdfff5d0f2c4f358975db3ab496495cf30ca1ca808f4f3488bdaccfa70d9'
