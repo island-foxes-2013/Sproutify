58660d282472ad2df06ab850b14ab38ef511e59d
Cole Pilegard <c.pilegard@gmail.com>
2013-08-16 12:04:51 -0700

    added tests and validations for status model

--- spec/models/status_spec.rb
+++ spec/models/status_spec.rb
@@ -1,5 +1,25 @@
 require 'spec_helper'
 
 describe Status do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it {should respond_to(:name)}
+  it {should respond_to(:supplies)}
+
+  let(:status) { FactoryGirl.build(:status) }
+
+  describe "#initialize" do
+
+    context "valid data" do
+      it "should save to database" do
+        status.save.should eq true
+      end
+    end
+
+    context "invalid data" do
+      it "should not save to database" do
+        status = Status.new
+        status.save.should eq false
+      end
+    end
+  end
 end
