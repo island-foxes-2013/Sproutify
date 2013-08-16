c1cf80f8f38652084de91756a1fe18c3fc964acb
dbcapprentice <adamloo85@gmail.com>
2013-08-16 11:17:52 -0700

    added first round of pending tests

---
+++ spec/features/landing_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "map generation" do
+
+  describe "data input" do
+    context "valid input" do
+      it "should generate a map" do
+        pending "check page for map"
+      end
+
+      it "should generate map with location input" do
+        pending "validate with lat, lng"
+      end
+
+      it "should generate a map with users in respective area" do
+        pending "validate with markers"
+      end
+    end
+
+    context "invalid input" do
+      it "should not generate a map" do
+        pending "page should not have a map and instead maybe an error message"
+      end
+    end
+  end
+end
