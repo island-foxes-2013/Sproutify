ea9225cc51d4f64db4073d350cc134ab9f184b50
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-16 21:57:45 -0700

    Removed erroneous map tests for landing page

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -6,25 +6,6 @@ describe "map generation" do
   end
 
   context "valid location", js:true do
-    before do
-      fill_in 'location', with: 'San Francisco'
-      click_button 'Submit'
-      sleep(3)
-    end
-
-    describe "map" do
-      it "should generate a map" do
-        page.should have_css('div.gm-style')
-      end
-
-      it "should generate map with correct location" do
-        pending "validate with lat, lng"
-      end
-
-      it "should generate a map with users in respective area" do
-        pending "validate with markers"
-      end
-    end
 
     describe "feed" do
       it "should list number of users in that area" do
@@ -37,9 +18,4 @@ describe "map generation" do
     end
   end
 
-  context "invalid input" do
-    it "should not generate a map" do
-      pending "page should not have a map and instead maybe an error message"
-    end
-  end
 end
