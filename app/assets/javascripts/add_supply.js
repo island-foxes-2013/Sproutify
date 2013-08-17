092b66d4ae1294bae3d03bf114aee4f9b3e64107
Gregory Varias <gregvarias@gmail.com>
2013-08-17 13:01:04 -0700

    User can add crops they have available and their respective statuses, and view a list of all available supplies

--- app/assets/javascripts/add_supply.js
+++ app/assets/javascripts/add_supply.js
@@ -1,7 +1,13 @@
 $(function() {
-	$('#add-supplies-form').on('ajax:success', function(){
-
-
-
+	$('#add-supplies-form').on('ajax:success', function(e, response, xhr, element){
+		$.ajax({
+			url: '/supplies',
+			type: 'GET'
+		}).done(function(response){
+			$('.user-supplies').empty();
+			$('.user-supplies').append(response.supply_partial)
+			$('form-control').val('');
+			// $('form-control:first-child').focus();
+		});
 	});
 });
--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -2,21 +2,21 @@ class SuppliesController < ApplicationController
 	skip_before_filter :require_login
 
 	def index
-		@supplies = User.find(params[:user_id].supplies.all
+		user = !params[:user_id].nil? ? User.find_by_id(params[:user_id]) : current_user
+		@supplies = user.supplies.all
+		render json: {
+		  supply_partial: render_to_string(partial:"shared/current_supply", locals: { current_supply: @supplies })
+		}
 	end
 
 
   def create
   	crop = Crop.find_or_create_by_name(params[:crop_name])
-  	supply = current_user.supplies.create(crop: crop)
+  	status = Status.create(name: params[:status])
+  	supply = current_user.supplies.create(crop: crop, status: status)
 
-  	# NOT USING 'STATUS' YET
-  	# status = Status.create(name: params[:status])
-  	# supply = current_user.supplies.create(crop: crop, status: status)
-  	
-  	render json: { supply: supply, crop: crop.name }
-
-  	# redirect_to supply_path(supply)
+  	puts "*" * 50
+  	render json: { supply: supply, crop: crop.name, status: status.name }
   end
 
   def show
