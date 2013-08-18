7046b98890800f8f819958207ca1956f9a9598a7
Gregory Varias <gregvarias@gmail.com>
2013-08-17 23:51:54 -0700

    select status via drop-down menu. user's supplies are separated depending on their status.

--- app/controllers/supplies_controller.rb
+++ app/controllers/supplies_controller.rb
@@ -5,20 +5,24 @@ class SuppliesController < ApplicationController
 
 	def index
 		user = !params[:user_id].nil? ? User.find_by_id(params[:user_id]) : current_user
-		supply_sentences = []
-		
+		growing = []
+		harvested = []
+		done = []
+
 		user.supplies.all.each do |supply|
 			crop = Crop.find_by_id(supply.crop_id)
 			status = Status.find_by_id(supply.status_id)
-			supply_sentences << "#{crop.name}'s current status is #{status ? status.name : 'empty'}"
+			growing << crop.name if status.name == 'growing'
+			harvested << crop.name if status.name == 'harvested'
+			done << crop.name if status.name == 'done'
 		end
-
-		render json: { supplies: supply_sentences }
+		
+		render json: { growing: growing, harvested: harvested, done: done }
 	end
 
   def create
-  	crop = Crop.find_or_create_by_name(params[:crop_name].first)
-  	status = Status.create(name: params[:status].first)
+  	crop = Crop.find_or_create_by_name(params[:crop_name])
+  	status = Status.find_or_create_by_name(params[:status_name])
   	supply = current_user.supplies.create(crop: crop, status: status)
   	render json: { supply: supply, crop: crop.name, status: status.name }
   end
