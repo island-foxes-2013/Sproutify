074d29901b5b6aebc7d9f281f0eae664367549c7
dbcapprentice <adamloo85@gmail.com>
2013-08-17 22:57:17 -0700

    added 5 page tests

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -1,33 +1,37 @@
 require 'spec_helper'
 
-describe "Location hook" do
+describe "Landing Page", js:true do
   before :each do
     visit root_path
   end
 
-  context "valid location", js:true do
+  subject {page}
+
+  it {should have_button('Check')}
+  it {should have_content('Find Gardeners near you')}
+  it {should have_field('zip')}
+  it {should have_field('address')}
+
+  context "when logged out" do
+    it {should have_link('Login')}
+  end
+  
+  context "valid location" do
+
     describe "feed" do
       let(:user) { FactoryGirl.create(:user) }
       # user.create_geocode(lat: 37.786453, lng: -122.418015)
 
       it "should list number of users in that area" do
-        expect do
-          fill_in "location", with: "San Francisco"
-            click_button "submit"
-            sleep(3)
-        end.to change{ page.has_content?("There are 1 gardeners in your area!")}
+        pending "need solr rspec connection"
       end
 
       it "should list what users in that area are growing" do
-        pending "can't implement without routes and DB"
+        pending "need solr rspec connection"
       end
 
       it "should list 0 users when no matches" do
-        pending "need to get capybara working"
-        # fill_in "location", with: "Hercules"
-        # click_button "submit"
-        # sleep(1)
-        # page.should have_content "There are 0 gardeners in your area!"
+        pending "need solr rspec connection"
       end
     end
   end
