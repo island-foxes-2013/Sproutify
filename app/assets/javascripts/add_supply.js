605b22e8c82e110ac98971ff55cd937bc42abb18
Gregory Varias <gregvarias@gmail.com>
2013-08-17 22:05:57 -0700

    started adding drop-down field for status

--- app/assets/javascripts/add_supply.js
+++ app/assets/javascripts/add_supply.js
@@ -5,10 +5,7 @@ $(function() {
 			type: 'GET'
 		}).done(function(response){
 			$('.user-supplies').empty();
-			console.log(response);
 			$('.user-supplies').html(HandlebarsTemplates['current_supply'](response))
-			// $('.user-supplies').append(response.supply_partial)
-
 			$('form-control').val('');
 		});
 	});
