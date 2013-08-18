c4461e5eee9d4dbbb00a8acf3d5f7c4dd8d52992
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-18 10:43:28 -0700

    ZEE DOIN HIS THANG

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,58 +1,73 @@
-////////////////////
-// LANDING MANAGER
-function LandingManager() {
-  $("#main-body").html(HandlebarsTemplates['landing']());
+(function() {
+  ////////////////////
+  // LANDING MANAGER
+  function LandingManager() {
+    $("#main-body").html(HandlebarsTemplates['landing']());
+    bindEvents();
+  }
 
-  $('body').on('click', '#submit', function(event) {
-    event.preventDefault();
-    var geocoder = new Geocoder();
-    geocoder.fetch(getLocation(), function(location) {
-      clearResults();
-      getLocalInfo(location, function() {
-        $('.signup-link').fadeIn(4000);
+  function bindEvents() {
+    $('body').on('click', '#submit', function(event) {
+      event.preventDefault();
+      var geocoder = new Geocoder();
+      geocoder.fetch(getLocation(), function(location) {
+        clearResults();
+        getLocalInfo(location, function() {
+          $('.signup-link').fadeIn(4000);
+        });
       });
     });
-  });
+  }
 
+  function clearResults() {
+    $('.user_count h3').remove();
+    $('.user_count h4').remove();
+    $('.available p').remove();
+    $('.demanded p').remove();
+  }
+
+  function getLocation(){
+    return $("#address").val() +', '+ $("#zip").val();
+  }
+
+  function setLocationFromPlugin(){
+    $('#location').val(geoplugin_city());
+  }
+
+  function getLocalInfo(location, callBack) {
+    var locationFetcher = new LocationFetcher();
+    locationFetcher.fetch(location, function(result) {
+      $('#hidden_lat').attr("value", location.lat);
+      $('#hidden_lng').attr("value", location.lng);
+      $('.user_count').append("<h3>There are "+ result.user_count +" gardeners near you!</h3>");
+      $('.user_count').hide().fadeIn();
+      if (result.user_count !== 0) {
+        $('.user_count').append("<h4>In your area,</h4>").hide().fadeIn(1000);
+
+        for (var i = 0; i < 5; i++) {
+          $('.available').append("<p>"+ result.crops_available[i].count+ " people have " + result.crops_available[i].name.toLowerCase() +" available!</p>").hide().fadeIn(2000);
+        }
+
+        for (var i = 0; i< 5; i++) {
+          $('.demanded').append("<p>"+ result.crops_demanded[i].count+ " people want " + result.crops_demanded[i].name.toLowerCase() + "!</p>").hide().fadeIn(2000);
+        }
+      }
+      callBack();
+    });
+  }
+
+  window.LandingManager = LandingManager;
+})();
+
+
+function LocationFetcher() {
 }
 
-function clearResults() {
-  $('.user_count h3').remove();
-  $('.user_count h4').remove();
-  $('.available p').remove();
-  $('.demanded p').remove();
-}
 
-function getLocation(){
-  return $("#address").val() +', '+ $("#zip").val();
-}
-
-function setLocationFromPlugin(){
-  $('#location').val(geoplugin_city());
-}
-
-function getLocalInfo(location, callBack) {
+LocationFetcher.prototype.fetch(location, callback) {
   $.ajax({
     url: "/fetch",
     type: "get",
     data: { lat: location.lat, lng: location.lng}
-  }).done(function(result) {
-
-    $('#hidden_lat').attr("value", location.lat);
-    $('#hidden_lng').attr("value", location.lng);
-    $('.user_count').append("<h3>There are "+ result.user_count +" gardeners near you!</h3>")
-    $('.user_count').hide().fadeIn();
-    if (result.user_count !== 0) {
-      $('.user_count').append("<h4>In your area,</h4>").hide().fadeIn(1000);
-
-      for (var i = 0; i < 5; i++) {
-        $('.available').append("<p>"+ result.crops_available[i].count+ " people have " + result.crops_available[i].name.toLowerCase() +" available!</p>").hide().fadeIn(2000);
-      }
-
-      for (var i = 0; i< 5; i++) {
-        $('.demanded').append("<p>"+ result.crops_demanded[i].count+ " people want " + result.crops_demanded[i].name.toLowerCase() + "!</p>").hide().fadeIn(2000);
-      }
-    }
-    callBack();
-  });
+  }).done(callback);
 }
