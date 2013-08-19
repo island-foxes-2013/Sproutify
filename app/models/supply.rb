89d2849a1fad777880b420a17ba9bda1a91f9262
Gregory Varias <gregvarias@gmail.com>
2013-08-18 17:53:07 -0700

    more cleanup in supply model

--- app/models/supply.rb
+++ app/models/supply.rb
@@ -5,13 +5,5 @@ class Supply < ActiveRecord::Base
   belongs_to :status
 
   validates_presence_of :user, :crop
-  # validates_presence_of :user, :crop, :status
-  # validates_uniqueness_of :user_id, scope: [:crop_id, :status_id]
   validates_uniqueness_of :user_id, scope: [:crop_id, :status_id]
-
-  def available?
-  end
-
-  def count
-  end
 end
