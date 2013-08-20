7f1d8f61f9360d9b088d17c1cfd7439077c55e7c
Cole Pilegard <c.pilegard@gmail.com>
2013-08-20 14:41:52 -0700

    added more tests to AllGardenSet

--- spec/javascripts/models/AllGardenSetSpec.js
+++ spec/javascripts/models/AllGardenSetSpec.js
@@ -25,5 +25,20 @@ describe("AllGardenSet", function() {
       $(garden).trigger('removed');
       expect(gardenSet.set).toNotContain(garden);
     });
+
+    it("should add supplies/demanded crop indices", function() {
+      gardenSet.addGarden(garden);
+      expect(gardenSet.demandCropIndex).toEqual({'corn':[0], 'potato':[0]});
+      expect(gardenSet.supplyCropIndex).toEqual({'beet':[0], 'bean':[0]});
+    });
+  });
+
+  describe("#clearIndex", function() {
+    it("should clear the indices", function() {
+      gardenSet.addGarden(garden);
+      gardenSet.clearIndex();
+      expect(gardenSet.demandCropIndex).toEqual({});
+      expect(gardenSet.supplyCropIndex).toEqual({});
+    });
   });
 });
