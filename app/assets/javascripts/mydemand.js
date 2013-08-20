f8afff855c9f02166d42d577937288ff1f46020a
michaelchihuyho <michaelchihuyho@gmail.com>
2013-08-20 13:01:11 -0700

    Changed myDemand to be able to render errors and made it more flexible. Untested

--- app/assets/javascripts/mydemand.js
+++ app/assets/javascripts/mydemand.js
@@ -27,18 +27,31 @@ MyDemand.prototype = {
 
 function MyDemandView(myDemand){
   this.myDemand = myDemand;
-  this.showForm();
+
+  this.linkSelector = ".myDemand-link";
+  this.formSelector = '#myDemand-form';
+  this.modalSelector = "#myDemand-modal";
+  this.$parent = $("body");
+  // this.$modal = $(this.modalSelector);
+  this.$form = $(this.formSelector);
+
+  this.appendForm();
   this.bindEvents();
 }
 
 MyDemandView.prototype = {
-  showForm: function(){
+  appendForm: function(){
     $('#add-demand').html(HandlebarsTemplates['add_demand']);
   },
   bindEvents: function(){
     var self = this;
-    $("body").on('ajax:success', '#add-demands-form', function(){
+    this.$parent.on('ajax:success', this.formSelector, function(event, response, xhr, element){
+      self.renderErrors(response);
       self.myDemand.getDemand();
     });
+  },
+  renderErrors: function(response){
+    this.$form.find('.alert').remove();
+    this.$form.find(".modal-body").prepend(HandlebarsTemplates['error'](response));
   }
 };
