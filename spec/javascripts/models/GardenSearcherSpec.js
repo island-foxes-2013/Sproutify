9b9549e8779644de900102a4c32715b9da7e34a6
Cole Pilegard <c.pilegard@gmail.com>
2013-08-20 16:59:53 -0700

    added a gardensearcher test

--- spec/javascripts/models/GardenSearcherSpec.js
+++ spec/javascripts/models/GardenSearcherSpec.js
@@ -1,7 +1,25 @@
 describe("GardenSearcher", function() {
-  var searcher;
+  var server, callback;
+  var user = {name: "somebody"};
+  var boundary = {ulat:2, ulng:2, blat:3, blng:0};
 
   beforeEach(function(){
-     
-  })
-})
+    server = sinon.fakeServer.create();
+  });
+
+  describe("#fetch", function() {
+    it("returns user objects within a boundary", function() {
+      var searcher = new GardenSearcher();
+      callback = function(results) {
+        expect(results[0].user).toEqual(user);
+      }
+
+      searcher.fetch(boundary, callback);
+    });
+  });
+
+  afterEach(function() {
+    server.restore();
+  });
+
+});
