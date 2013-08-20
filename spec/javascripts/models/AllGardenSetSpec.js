52f744aa2282b0fb7e1d98017ff62d8cc8260bc6
Cole Pilegard <c.pilegard@gmail.com>
2013-08-20 14:32:51 -0700

    added trigger event to AllGardenSet test

--- spec/javascripts/models/AllGardenSetSpec.js
+++ spec/javascripts/models/AllGardenSetSpec.js
@@ -3,7 +3,27 @@ describe("AllGardenSet", function() {
   var garden;
 
   beforeEach(function() {
-    garden = new Garden();
+    garden = new Garden({demands:[{name:'corn'}, {name:'potato'}],
+                         supplies:[{name:'beet'}, {name:'bean'}]});
     gardenSet = new AllGardenSet();
+  });
+
+  it("should create an empty array of gardens and crop indices", function() {
+    expect(gardenSet.set).toEqual([]);
+    expect(gardenSet.demandCropIndex).toEqual({});
+    expect(gardenSet.supplyCropIndex).toEqual({});
+  });
+
+  describe("#addGarden", function() {
+    it("should add a garden to the set", function() {
+      gardenSet.addGarden(garden);
+      expect(gardenSet.set).toContain(garden);
+    });
+
+    it("should bind the garden to a 'removed' event", function() {
+      gardenSet.addGarden(garden);
+      $(garden).trigger('removed');
+      expect(gardenSet.set).toNotContain(garden);
+    });
   });
 });
