d4119430e4ad2c42534f00da58f89df103b9a92c
dbcapprentice <adamloo85@gmail.com>
2013-08-21 12:15:24 -0700

    working on main_feed spec

--- spec/features/main_feed.rb
+++ spec/features/main_feed.rb
@@ -11,12 +11,25 @@ end
 describe "main page", js:true do
   include SolrSpecHelper
   let(:user) {FactoryGirl.create(:user)}
+  let(:crop) {FactoryGirl.create(:crop)}
+  let(:status) {FactoryGirl.create(:status)}
 
   before do
     user.create_geocode(lat: 38.024025, lng: -122.290589)
+    p crop
+    Supply.create(user: user, crop: crop, status: status)
+    p user.supplies
     Sunspot.commit
     login(user)
     solr_setup
+  end
+
+  context "user supplies" do
+    it "display user supplies" do
+      click_link 'share_link'
+      page.should have_content(crop.name)
+      user.supplies.count.should eq 1
+    end
   end
 
   it "should have load the navigation" do
@@ -25,19 +38,20 @@ describe "main page", js:true do
     page.should have_link('messages-nav', href: '/inbox')
   end
 
+
   context "supply/demand control" do
 
     it "should load" do
       page.should have_content("What produce do you have available?")
     end
 
-    it "should write and display updated supplies" do
-      # fill_in "supply_crop_name", with: "orange"
-      # click_button "Add Supply"
-      # click_link 'share_link'
-      # page.should have_content("oranges")
-      # user.demands.count.should eq 1
-      pending "see notes above"
+    context "user supplies" do
+
+      it "display user supplies" do
+        click_link 'share_link'
+        page.should have_content(crop.name)
+        user.supplies.count.should eq 1
+      end
     end
   end
 end
