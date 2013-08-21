259ac33bcaae9cefd4fc6ead5795196726dd2b98
Cole Pilegard <c.pilegard@gmail.com>
2013-08-20 22:41:13 -0700

    So my goal was to have the markers not reset every time the map moved. After breaking everything multiple times, cursing God, questioning my reasons for wanting to program, the damn thing works. I can not explain what I did between this and the last commit.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -12,16 +12,40 @@ Map.prototype = {
     this.gardens.addGarden(garden);
   },
   refreshGardens: function(boundary){
-    this.clearGardens();
+    //remove gardens out of bounds
+    var gardens_to_be_removed = [];
+    $.each(this.gardens.set, function(index,garden) {
+      if ((garden.attrs.lat < boundary.ulat || garden.attrs.lat > boundary.blat) || (garden.attrs.lng < boundary.ulng || garden.attrs.lng > boundary.blng)) {
+        gardens_to_be_removed.push(garden);
+      }
+    });
+    $.each(gardens_to_be_removed, function() {
+      this.remove();
+    });
+    
     var self = this;
     this.searcher.fetch(boundary, function(gardens) {
-      //
-      self.gardens.clearIndex();
+      // self.gardens.clearIndex();
+      var found_match = false;
+      var found_object = false;
       $.each(gardens, function() {
-        self.placeGarden(this);
+        contender = this;
+        $.each(self.gardens.set, function() {
+          if (this.attrs.lat === contender.attrs.lat) {
+            found_match = true;
+            found_object = this;
+            console.log("found match");
+          }
+        });
+        if (!found_match) {
+          self.placeGarden(this);
+        } else {
+          found_match = false;
+          found_object = false;
+        }
       });
+      self.gardens.refreshIndexes();
     });
-    console.log(this.gardens)
   },
   clearGardens: function() {
     var self = this;
