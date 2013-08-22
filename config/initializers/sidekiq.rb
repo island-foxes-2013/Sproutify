e5607db5a05c74fdaa02c8e35821e73c60665da4
Cole Pilegard <c.pilegard@gmail.com>
2013-08-21 17:50:02 -0700

    be sure to configure both client and server\!

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
-p "x" * 25
 if Rails.env == "production"
-  p "*" * 25
-  p ENV['REDISCLOUD_URL']
   Sidekiq.configure_client do |config|
+    config.redis = { :url => ENV['REDISCLOUD_URL'], :namespace => 'sproutify' }
+  end
+  Sidekiq.configure_server do |config|
     config.redis = { :url => ENV['REDISCLOUD_URL'], :namespace => 'sproutify' }
   end
 end
